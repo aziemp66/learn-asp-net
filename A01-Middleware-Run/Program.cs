c40c58a8e8e541ff349e1ecde45dcbd2896d9094
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-21 13:10:37 +0700

    Complete: Run

--- A01-Middleware-Run/Program.cs
+++ A01-Middleware-Run/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(
+    async (HttpContext context) =>
+    {
+        await context.Response.WriteAsJsonAsync(
+            new { Message = "Hello World", Sender = "Xi Jin Ping" }
+        );
+    }
+);
+
+app.Run(
+    async (HttpContext context) =>
+    {
+        await context.Response.WriteAsJsonAsync(
+            new { Message = "Hello Again", Sender = "Xi Jin Ping" }
+        );
+    }
+);
 
 app.Run();
