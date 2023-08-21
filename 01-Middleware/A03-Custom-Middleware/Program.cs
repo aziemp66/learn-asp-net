fa4beada7c658dd1b26ab5c2fadd5916e74370eb
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-22 00:09:59 +0700

    Custom Middleware Class

--- 01-Middleware/A03-Custom-Middleware/Program.cs
+++ 01-Middleware/A03-Custom-Middleware/Program.cs
@@ -1,6 +1,39 @@
+using A03_Custom_Middleware.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<ApiRequestMiddleware>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Middleware 1
+app.Use(
+    async (ctx, next) =>
+    {
+        await ctx.Response.WriteAsync("Hello 1\n");
+        await next(ctx);
+        await ctx.Response.WriteAsync("Hello 6\n");
+    }
+);
+
+// Middleware 2
+app.UseMiddleware<ApiRequestMiddleware>();
+
+// Middleware 3
+app.Use(
+    async (ctx, next) =>
+    {
+        await ctx.Response.WriteAsync("Hello 3\n");
+        await next(ctx);
+        await ctx.Response.WriteAsync("Hello 4\n");
+    }
+);
+
+app.MapGet(
+    "/",
+    async (ctx) =>
+    {
+        await ctx.Response.WriteAsync("Hemlo World\n");
+    }
+);
 
 app.Run();
